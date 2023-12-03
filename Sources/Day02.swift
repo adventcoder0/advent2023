de1cd760413deb20fc66ed46f40551b5b13462df
adventcoder0 <finalfantasyadventcoder+github@gmail.com>
2023-12-02 21:33:17 -0600

    Day 1 All Done

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -1,6 +1,6 @@
 import Algorithms
 
-struct Day01: AdventDay {
+struct Day02: AdventDay {
     // Save your data in a corresponding text file in the `Data` directory.
     var data: String
 
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -13,13 +13,23 @@ final class Day01Tests: XCTestCase {
     treb7uchet
     """
 
+    let testDataPart2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
     func testPart1() throws {
         let challenge = Day01(data: testData)
         XCTAssertEqual(String(describing: challenge.part1()), "142")
     }
 
     func testPart2() throws {
-        let challenge = Day01(data: testData)
-        XCTAssertEqual(String(describing: challenge.part2()), "32000")
+        let challenge = Day01(data: testDataPart2)
+        XCTAssertEqual(String(describing: challenge.part2()), "281")
     }
 }
