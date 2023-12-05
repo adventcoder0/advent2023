dc19a5c2453caddbe71cbf5df0f77246260e4592
adventcoder0 <finalfantasyadventcoder+github@gmail.com>
2023-12-05 12:23:14 -0600

    Day 4 Part2 done

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -26,18 +26,35 @@ struct Day04: AdventDay {
 
     // Replace this with your solution for the last part of the day's challenge.
     func part2() -> Any {
-        var myCollection = CardCollection(originals: [])
+        var numMatches: [Int] = []
+        var numCards: [Int] = Array(repeating: 1, count: entities.count)
         entities.forEach {
             lineString in
             let halves = lineString.split(separator: "|")
             let winingNumbers: [Int] = halves[0].split(separator: ":").last!.split(separator: " ").map { Int($0)! }
             let holdingNumbers: [Int] = halves[1].split(separator: " ").map { Int($0)! }
-            let original = Card(winningNumbers: winingNumbers, holdingNumbers: holdingNumbers)
-            myCollection.addOriginal(original: original)
+            numMatches.append(Card(winningNumbers: winingNumbers, holdingNumbers: holdingNumbers).getMatches().count)
         }
 
-        myCollection.domainExpansion()
-        return myCollection.totalCards()
+        for cardNumber in 0 ..< numCards.count {
+            let multiplier = numCards[cardNumber]
+            // print("Number of cards for Card \(cardNumber + 1): \(multiplier)")
+            // print("This card should propagate down to \(cardNumber + 1 + numMatches[cardNumber])")
+            let depth = numMatches[cardNumber]
+            if depth == 0 {
+                continue
+            }
+            for i in 1 ... depth {
+                // let cardToAdd = cardNumber + i
+                // print("addingto \(cardToAdd + 1)")
+                numCards[cardNumber + i] += multiplier
+            }
+            // print("state: \(numCards)")
+            // print("---------------------")
+        }
+        // print("endList: \(numCards)")
+
+        return numCards.reduce(0,+)
     }
 }
 
@@ -51,67 +68,5 @@ struct Card {
 
     func getWorth() -> Int {
         return getMatches().isEmpty ? 0 : NSDecimalNumber(decimal: pow(2, getMatches().count - 1)).intValue
-    }
-
-    func copy() -> Card {
-        return Card(winningNumbers: winningNumbers, holdingNumbers: holdingNumbers)
-    }
-}
-
-struct CardCollection {
-    var originals: [Card]
-    var copies: [Int: [Card]] = [:]
-
-    mutating func domainExpansion() {
-        for i in 0 ..< originals.count {
-            // print("-----------------start")
-            // print("working on card: \(i + 1)")
-            cardExpansion(from: originals[i], currentIndex: i)
-            if copies[i] != nil {
-                // print("Copies for card: \(i + 1)")
-
-                for item in copies[i]! {
-                    cardExpansion(from: item, currentIndex: i)
-                }
-
-                // print("---------copies end-----")
-            }
-            // print("-----------------end")
-        }
-    }
-
-    mutating func cardExpansion(from: Card, currentIndex: Int) {
-        let numCopies = from.getMatches().count
-
-        if numCopies == 0 {
-            return
-        }
-
-        for i in 1 ... numCopies {
-            // print("made a copy of card #: \(currentIndex + i + 1)")
-            makeCopies(copyIndex: currentIndex + i)
-        }
-    }
-
-    mutating func makeCopies(copyIndex: Int) {
-        let newCard = originals[copyIndex].copy()
-        var copyList = copies[copyIndex]
-        if copyList == nil {
-            copies[copyIndex] = [newCard]
-        } else {
-            copyList!.append(newCard)
-            copies[copyIndex] = copyList
-        }
-    }
-
-    mutating func addOriginal(original: Card) {
-        originals.append(original)
-    }
-
-    func totalCards() -> Int {
-        return copies.reduce(0) {
-            sum, dictEntry in
-            sum + dictEntry.value.count
-        } + originals.count
     }
 }
